4385733dd4f1da072426252736d240ee0115f246
Your Name <you@example.com>
2026-04-29 10:21:46 +0800

    新增PWA模式

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Order Cafe | 點咖啡',
+        short_name: 'Order Cafe',
+        description: 'Single Origin Coffee Ordering App',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
   base: '/order-cafe/',
 })
