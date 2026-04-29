32c35cf1486549359553b785aaacb5bf3a1ca69b
Your Name <you@example.com>
2026-04-29 10:34:38 +0800

    修改縮圖

--- vite.config.ts
+++ vite.config.ts
@@ -16,14 +16,25 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: 'favicon.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
           },
           {
-            src: 'favicon.svg',
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
